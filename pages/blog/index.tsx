e7d11a160ac2a7394b7e04c868445b7085597458
João Victor do Nascimento <nascimentojoaovictor21@gmail.com>
2023-06-21 17:36:26 -0300

    create blog and store pages

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import Container from '../../components/container'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <Container>
+        <div className='mt-4 mb-4'>
+          <h1 className="text-3xl font-bold mb-4">Bem vindo ao blog</h1>
+          <p className='mb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita quisquam voluptate inventore sint laboriosam nemo sapiente ea quae quam exercitationem placeat, tempora veritatis repudiandae eius eum nisi recusandae, dolorem porro?</p>
+          <Image 
+            className='w-full px-4'
+            alt='Imagem Hero' 
+            src="/images/jm-2katanas-4-full-hd.jpg" 
+            width={300} 
+            height={200}
+          />
+        </div>
+        <div className="flex justify-center">
+          <div className="flex flex-col">
+            <Link href='/blog/post'>
+              <Image
+                className='w-full mb-4'
+                src="/images/jm-2katanas-4-full-hd.jpg"
+                width={300}
+                height={200}
+                alt='primeiro-post'
+                />
+            </Link>
+            <span className="text-pink-900 mb-4">Tag</span>
+            <Link href='/blog/post'>
+              <h2 className="text-xl font-bold mb-4 hover:text-pink-900">Primeiro post</h2>
+            </Link>
+            <span className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, tempore mollitia? Provident minus asperiores repellendus exercitationem totam optio unde? Modi voluptate excepturi quo officiis optio exercitationem hic vitae deserunt in?</span>
+            <span className="text-xs mb-4">21/06/2023</span>
+            </div>
+        </div>
+      </Container>
+    </>
   )
 }
--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,7 +1,44 @@
+import Head from 'next/head'
 import React from 'react'
+import Container from '../../components/container'
+import Image from 'next/image'
+import { Star } from '@phosphor-icons/react'
+import Link from 'next/link'
 
 export default function Store() {
   return (
-    <div>Store</div>
+    <>
+      <Head>
+        <title>Store</title>
+      </Head>
+      <Container>
+        <div className="mt-4 mb-4">
+          <h1 className="text-3xl font-bold">Bem-vindo a nossa loja!</h1>
+          <p className="mb-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat dolores sequi expedita vitae culpa obcaecati incidunt blanditiis nesciunt natus error quaerat, odio, inventore dolorum eligendi numquam. Id facilis nobis consequatur.</p>
+        </div>
+        <div className="mb-4 px-4">
+          <div className="flex justify-end">
+            <span className="border text-xs px-2 py-1 mb-2">10%</span>
+          </div>
+          <Link href='/store/product'>
+            <Image 
+              className='w-full mb-2'
+              alt='Imagem Hero' 
+              src="/images/jtae-me-jm-seannoanimacion2-blend-0001-121513.jpg" 
+              width={300} 
+              height={200}
+            />
+            <div className="flex justify-between mb-2">
+              <h3 className="font-bold text-xl">Produto</h3>
+              <span className="flex gap-x-2">
+                <Star size={20} />
+                4,5
+              </span>
+            </div>
+            <span className='mb-2'>R$ 200,00</span>
+          </Link>
+        </div>
+      </Container>
+    </>
   )
 }
