ea2f8f5e79fbe6f07b9757997d44b45fb02c4a01
João Victor do Nascimento <36607540+indianous@users.noreply.github.com>
2023-07-22 19:07:19 -0300

    Atualizar o index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,42 @@ export default function Index({}: IndexProps) {
         <title>Soccobos</title>
       </Head>
       <Layout>
-        <article className="overflow-hidden rounded-lg shadow transition px-5 hover:shadow-lg">
+<section className="bg-gray-50 px-5 mb-4">
+  <div
+    className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+  >
+    <div className="mx-auto max-w-xl text-center">
+      <h1 className="text-3xl font-extrabold sm:text-5xl">
+        Understand User Flow.
+        <strong className="font-extrabold text-red-700 sm:block">
+          Increase Conversion.
+        </strong>
+      </h1>
+
+      <p className="mt-4 sm:text-xl/relaxed">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+        tenetur fuga ducimus numquam ea!
+      </p>
+
+      <div className="mt-8 flex flex-wrap justify-center gap-4">
+        <a
+          className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+          href="/get-started"
+        >
+          Get Started
+        </a>
+
+        <a
+          className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+          href="/about"
+        >
+          Learn More
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+        <article className="overflow-hidden rounded-lg shadow transition px-5 mb-4 hover:shadow-lg">
   <img
     alt="Office"
     src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
