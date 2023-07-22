dbdafe51c37f1d3cbeccef651bd1f15bf5c5391a
João Victor do Nascimento <36607540+indianous@users.noreply.github.com>
2023-07-22 19:05:38 -0300

    Atualizar o index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Index({}: IndexProps) {
         <title>Soccobos</title>
       </Head>
       <Layout>
-        <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
+        <article className="overflow-hidden rounded-lg shadow transition px-5 hover:shadow-lg">
   <img
     alt="Office"
     src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
